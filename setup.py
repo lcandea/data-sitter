1650be6a8feeee553f7913d6ec2ee02fc4f1c6ef
Lazaro Candea <lazaro@candea.es>
2025-03-13 17:05:28 +0000

    Slightly downgrade of pydantic for it to work in WASM. Added libraries to build and publish to pypi

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='data-sitter',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         # Keep this in sync with pyproject.toml
         "python-dotenv==1.0.1",
         "parse_type==0.6.4",
-        "pydantic==2.10.6",
+        "pydantic==2.10.5",
     ],
     entry_points={
         'console_scripts': [
-        'data-sitter=data_sitter.cli:main',
+            'data-sitter=data_sitter.cli:main',
         ],
     },
 )
