0a6940fd2958dba5c983f47c24a4e59cf8091527
Lazaro Candea <lazaro@candea.es>
2025-03-13 15:05:51 +0000

    Added dependencies to pyproject and setup

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
+        # Keep this in sync with pyproject.toml
         "python-dotenv==1.0.1",
         "parse_type==0.6.4",
         "pydantic==2.10.6",
